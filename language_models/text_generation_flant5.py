7588e49a056824844e42ed63b6211375faf77a78
ElMagoRubio <yanoleomicorreo@gmail.com>
2025-04-01 12:54:46 +0200

    Update text_generation_flant5.py
    Eliminada la carga del modelo desde el propio archivo

--- language_models/text_generation_flant5.py
+++ language_models/text_generation_flant5.py
@@ -1,3 +1,4 @@
+from llm_loader import get_model_and_tokenizer_from_index
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 import os, sys, time, torch
 
@@ -5,21 +6,11 @@ if (len(sys.argv) != 2):
     print("ERROR: Número de argumentos incorrecto.\nFormato: (./text_generation_flant5.py) (texto_entrada_usuario)")
     exit(1)
 
-# Obtener la ruta absoluta del directorio donde está este script
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-# Rutas locales del modelo y el tokenizador
-model_path = os.path.join(BASE_DIR, "model/google_flan-t5-large")
-tokenizer_path = os.path.join(BASE_DIR, "tokenizer/google_flan-t5-large")
+# Detectar si hay GPU disponible
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 # Cargar modelo y tokenizador
-tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
-model = AutoModelForSeq2SeqLM.from_pretrained(model_path)
-
-# Verificar que se recibió un argumento válido
-if len(sys.argv) < 2:
-    print("Error: No se recibió una pregunta como argumento.", flush=True)
-    sys.exit(1)
+tokenizer, model = get_model_and_tokenizer_from_index(1)
 
 context = "Responde como un aldeano medieval llamado Flanagan."
 
@@ -29,7 +20,7 @@ prompt = sys.argv[1].strip()
 input_text = f"Context: {context}\nPrompt: {prompt}"
 
 # Tokenizar la entrada
-inputs = tokenizer(input_text, return_tensors="pt")
+inputs = tokenizer(input_text, return_tensors="pt").to(device)
 
 # Inicio de medición del tiempo de generación de respuesta
 start_time = time.time()
