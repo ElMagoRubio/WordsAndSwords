9fdfa8c791c8cda4625efa6960d11afae2adbbdb
ElMagoRubio <yanoleomicorreo@gmail.com>
2025-04-03 11:13:58 +0200

    Borrada carga de model en smol

--- language_models/text_generation_smol.py
+++ language_models/text_generation_smol.py
@@ -1,20 +1,12 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
-import os, sys, time, torch
+from llm_loader import get_model_and_tokenizer_from_index
+import sys, time, torch
 
 if (len(sys.argv) != 2):
     print("ERROR: Número de argumentos incorrecto.\nFormato: (./text_generation_smol.py) (texto_entrada_usuario)")
     exit(1)
 
-# Obtener la ruta absoluta del directorio donde está este script
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-# Rutas locales del modelo y el tokenizador
-model_path = os.path.join(BASE_DIR, "model/HuggingFaceTB_SmolLM2-360M-Instruct")
-tokenizer_path = os.path.join(BASE_DIR, "tokenizer/HuggingFaceTB_SmolLM2-360M-Instruct")
-
 # Cargar modelo y tokenizador
-tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
-model = AutoModelForCausalLM.from_pretrained(model_path)
+tokenizer, model = get_model_and_tokenizer_from_index(2)
 
 # Definir el contexto y la pregunta
 context = "Responde como un aldeano medieval llamado Smol. Genera una sola respuesta de menos de 50 palabras en español."
