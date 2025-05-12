f8ebf5353cd4023b07f4316e8482531a73a6b048
ElMagoRubio <yanoleomicorreo@gmail.com>
2025-05-12 19:19:59 +0200

    Create close_server_request.py
    Creado archivo de petición de cierre al servidor

---
+++ server/close_server_request.py
@@ -0,0 +1,31 @@
+import json, socket
+
+# Dirección del servidor
+HOST = "127.0.0.1"
+PORT = 5005
+
+try:
+    # Crear socket de cliente
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((HOST, PORT))
+
+    # Crear petición
+    request = {
+        "text": "CLOSE_SERVER",
+        "model": "--"
+    }
+
+    client_socket.send(json.dumps(request).encode())
+
+    # Recibir respuesta
+    response = client_socket.recv(4096).decode()
+    client_socket.close()
+
+    result = json.loads(response)
+
+    print(json.dumps(result))
+
+except Exception as e:
+    print(json.dumps({
+    "response": "Error de cierre de servidor"
+}))
